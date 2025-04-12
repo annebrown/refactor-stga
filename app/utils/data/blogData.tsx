80351add79c8bb655cc64779532332f52c775dc9
Anne Brown <anne@browndomain.com>
2025-04-12 14:04:16 -0400

    Blogs Ready to Edit

--- app/utils/data/blogData.tsx
+++ app/utils/data/blogData.tsx
@@ -16,19 +16,19 @@ export const blogData = [
         blogURL: "/blog/posts/blogPage00XX"
     },
     */
-    // {
-    //     id: 19,
-    //     title: "'Call of the Forest' The Forgotten Wisdom of Trees",
-    //     paragraph:
-    //       "Come out to watch this beautiful film on the significance of ancient forests!",
-    //     image: "/images/blog/blog0019/blog19.png",
-    //     alt: "Forest with text: Call of the Forest, A film by Jeff McKay, and other details",
-    //     width: 353,
-    //     height: 382,
-    //     tags: ["Event"],
-    //     publishDate: "March 31, 2025",
-    //     blogURL: "/blog/posts/blogPage0019"
-    // },
+    {
+        id: 19,
+        title: "'Call of the Forest' The Forgotten Wisdom of Trees",
+        paragraph:
+          "Come out to watch this beautiful film on the significance of ancient forests!",
+        image: "/images/blog/blog0019/blog19.png",
+        alt: "Forest with text: Call of the Forest, A film by Jeff McKay, and other details",
+        width: 353,
+        height: 382,
+        tags: ["Event"],
+        publishDate: "March 31, 2025",
+        blogURL: "/blog/posts/post-2025-03-31-00"
+    },
     {
         id: 18,
         title: "Election day: Candidates weigh in on protecting Gillies Grove",
@@ -42,178 +42,178 @@ export const blogData = [
         publishDate: "February 27, 2025",
         blogURL: "/blog/posts/post-2025-02-27-00"
     },
-    // {
-    //     id: 17,
-    //     title: "Community Members Rally",
-    //     paragraph:
-    //       "Committee of Adjustment Approves Severance of Environmentally and Culturally Sensitive Lands",
-    //     image: "/images/blog/blog0017/blog17.png",
-    //     alt: "Local community gathering in gymnasium",
-    //     width: 500,
-    //     height: 278,
-    //     tags: ["UPDATE"],
-    //     publishDate: "February 14, 2025",
-    //     blogURL: "/blog/posts/blogPage0017"
-    // },
-    // {
-    //     id: 16,
-    //     title: "A Crucial Public Meeting to Sever Land",
-    //     paragraph:
-    //       "We need everyone present for this one!",
-    //     image: "/images/blog/blog0016/blog16.png",
-    //     alt: "Nick Smith Centre Entrance",
-    //     width: 500,
-    //     height: 325,
-    //     tags: ["ACTION"],
-    //     publishDate: "February 1, 2025",
-    //     blogURL: "/blog/posts/blogPage0016"
-    // },
-    // {
-    //     id: 15,
-    //     title: "Community Stands up for Gillies Grove",
-    //     paragraph:
-    //       "Ecological Study Needed",
-    //     image: "/images/blog/blog0015/blog15.png",
-    //     alt: "Logo and Mary Reynolds Quote",
-    //     width: 500,
-    //     height: 279,
-    //     tags: ["UPDATE"],
-    //     publishDate: "January 29, 2025",
-    //     blogURL: "/blog/posts/blogPage0015"
-    // },
-    // {
-    //     id: 14,
-    //     title: "STGA to Present Delegation at Town Hall",
-    //     paragraph:
-    //       "Your Presence Matters",
-    //     image: "/images/blog/blog0014/blog14.jpg",
-    //     alt: "Arnprior Municipal Offices Front Entrance",
-    //     width: 750,
-    //     height: 402,
-    //     tags: ["ACTION"],
-    //     publishDate: "January 27, 2025",
-    //     blogURL: "/blog/posts/blogPage0014"
-    // },
-    // {
-    //     id: 13,
-    //     title:          "Join the Call for Council to Take Action",
-    //     paragraph:      "Announcing our NEW Postcard Campaign!",
-    //     image:          "/images/blog/blog0013/blog13.png",
-    //     alt: "Greetings from Gillies Grove, 10,000 years in the making",
-    //     width:          500,
-    //     height:         386,
-    //     tags:           ["ACTION"],
-    //     publishDate:    "January 18, 2025",
-    //     blogURL:        "/blog/posts/blogPage0013"
-    // },
-    // {
-    //     id: 12,
-    //     title: "'Don't It Always Seem to Go ...'",
-    //     paragraph:
-    //       "Let's rewrite this song together!",
-    //     image: "/images/blog/blog0012/blog12.webp",
-    //     alt: "Old grown",
-    //     width: 500,
-    //     height: 375,
-    //     tags: ["Event"],
-    //     publishDate: "January 16, 2025",
-    //     blogURL: "/blog/posts/blogPage0012"
-    // },
-    // {
-    //     id: 11,
-    //     title: "A Call to the Community",
-    //     paragraph:
-    //       "STGA Chair Lacey Smith speaks from the heart",
-    //     image: "/images/blog/blog0011/blog11.webp",
-    //     alt: "Child stands on large tree stump in wintery forest",
-    //     width: 500,
-    //     height: 375,
-    //     tags: ["ACTION"],
-    //     publishDate: "January 6, 2024",
-    //     blogURL: "/blog/posts/blogPage0011"
-    // },
-    // {
-    //     id: 10,
-    //     title: "Biodiversity Tour Success",
-    //     paragraph:
-    //       "amid a snowy backdrop, the diversity of this incredible land shone through!",
-    //     image: "/images/blog/blog0010/blog10.svg",
-    //     alt: "Concerned local citizens join together in a beautiful wintery woodland setting.",
-    //     width: 500,
-    //     height: 450,
-    //     tags: ["UPDATE"],
-    //     publishDate: "December 19, 2024",
-    //     blogURL: "/blog/posts/blogPage0010"
-    // },
+    {
+        id: 17,
+        title: "Community Members Rally",
+        paragraph:
+          "Committee of Adjustment Approves Severance of Environmentally and Culturally Sensitive Lands",
+        image: "/images/blog/blog0017/blog17.png",
+        alt: "Local community gathering in gymnasium",
+        width: 500,
+        height: 278,
+        tags: ["UPDATE"],
+        publishDate: "February 14, 2025",
+        blogURL: "/blog/posts/post-2025-02-14-00"
+    },
+    {
+        id: 16,
+        title: "A Crucial Public Meeting to Sever Land",
+        paragraph:
+          "We need everyone present for this one!",
+        image: "/images/blog/blog0016/blog16.png",
+        alt: "Nick Smith Centre Entrance",
+        width: 500,
+        height: 325,
+        tags: ["ACTION"],
+        publishDate: "February 1, 2025",
+        blogURL: "/blog/posts/post-2025-01-06-00"
+    },
+    {
+        id: 15,
+        title: "Community Stands up for Gillies Grove",
+        paragraph:
+          "Ecological Study Needed",
+        image: "/images/blog/blog0015/blog15.png",
+        alt: "Logo and Mary Reynolds Quote",
+        width: 500,
+        height: 279,
+        tags: ["UPDATE"],
+        publishDate: "January 29, 2025",
+        blogURL: "/blog/posts/post-2025-01-29-00"
+    },
+    {
+        id: 14,
+        title: "STGA to Present Delegation at Town Hall",
+        paragraph:
+          "Your Presence Matters",
+        image: "/images/blog/blog0014/blog14.jpg",
+        alt: "Arnprior Municipal Offices Front Entrance",
+        width: 750,
+        height: 402,
+        tags: ["ACTION"],
+        publishDate: "January 27, 2025",
+        blogURL: "/blog/posts/post-2025-01-27-00"
+    },
+    {
+        id: 13,
+        title:          "Join the Call for Council to Take Action",
+        paragraph:      "Announcing our NEW Postcard Campaign!",
+        image:          "/images/blog/blog0013/blog13.png",
+        alt: "Greetings from Gillies Grove, 10,000 years in the making",
+        width:          500,
+        height:         386,
+        tags:           ["ACTION"],
+        publishDate:    "January 18, 2025",
+        blogURL:        "/blog/posts/post-2025-01-18-00"
+    },
+    {
+        id: 12,
+        title: "'Don't It Always Seem to Go ...'",
+        paragraph:
+          "Let's rewrite this song together!",
+        image: "/images/blog/blog0012/blog12.webp",
+        alt: "Old grown",
+        width: 500,
+        height: 375,
+        tags: ["Event"],
+        publishDate: "January 16, 2025",
+        blogURL: "/blog/posts/post-2025-01-16-00"
+    },
+    {
+        id: 11,
+        title: "A Call to the Community",
+        paragraph:
+          "STGA Chair Lacey Smith speaks from the heart",
+        image: "/images/blog/blog0011/blog11.webp",
+        alt: "Child stands on large tree stump in wintery forest",
+        width: 500,
+        height: 375,
+        tags: ["ACTION"],
+        publishDate: "January 6, 2024",
+        blogURL: "/blog/posts/post-2025-01-06-00"
+    },
+    {
+        id: 10,
+        title: "Biodiversity Tour Success",
+        paragraph:
+          "amid a snowy backdrop, the diversity of this incredible land shone through!",
+        image: "/images/blog/blog0010/blog10.svg",
+        alt: "Concerned local citizens join together in a beautiful wintery woodland setting.",
+        width: 500,
+        height: 450,
+        tags: ["UPDATE"],
+        publishDate: "December 19, 2024",
+        blogURL: "/blog/posts/post-2024-12-19-00"
+    },
 
-    // {
-    //     id: 9,
-    //     title: "Gillies Grove Biodiversity Tour",
-    //     paragraph:
-    //       "Walk the Grove and learn more about this renowned ecosystem",
-    //     image: "/images/blog/blog0009/blog9.webp",
-    //     alt: "Autumn Forest",
-    //     width: 500,
-    //     height: 375,
-    //     tags: ["EVENT"],
-    //     publishDate: "December 4, 2024",
-    //     blogURL: "/blog/posts/blogPage0009"
-    // },
+    {
+        id: 9,
+        title: "Gillies Grove Biodiversity Tour",
+        paragraph:
+          "Walk the Grove and learn more about this renowned ecosystem",
+        image: "/images/blog/blog0009/blog9.webp",
+        alt: "Autumn Forest",
+        width: 500,
+        height: 375,
+        tags: ["EVENT"],
+        publishDate: "December 4, 2024",
+        blogURL: "/blog/posts/post-2024-12-04-00"
+    },
     
-    // {
-    //     id: 8,
-    //     title: "Response to Cavanagh\'s Announcement",
-    //     paragraph:
-    //       "STGA is not surprised, not deterred",
-    //     image: "/images/blog/blog0008/blog8.webp",
-    //     alt: "Sundrenched Woodland Path",
-    //     width: 500,
-    //     height: 280,
-    //     tags: ['IMPORTANT NEWS'],
-    //     publishDate: "October 31, 2024",
-    //     blogURL: "/blog/posts/blogPage0008"
-    // },
+    {
+        id: 8,
+        title: "Response to Cavanagh\'s Announcement",
+        paragraph:
+          "STGA is not surprised, not deterred",
+        image: "/images/blog/blog0008/blog8.webp",
+        alt: "Sundrenched Woodland Path",
+        width: 500,
+        height: 280,
+        tags: ['IMPORTANT NEWS'],
+        publishDate: "October 31, 2024",
+        blogURL: "/blog/posts/post-2024-10-31-00"
+    },
 
-    // {
-    //     id: 7,
-    //     title: "Not a Done Deal!",
-    //     paragraph:
-    //       "We don’t want a housing development beside and cutting into the Grove.",
-    //     image: "/images/blog/blog0007/blog7.png",
-    //     alt: "People walking a woodland path",
-    //     width: 500,
-    //     height: 270,
-    //     tags: ["UPDATE"],
-    //     publishDate: "October 17, 2024",
-    //     blogURL: "/blog/posts/blogPage0007"
-    // },
-    // {
-    //     id: 6,
-    //     title: "Giving Thanks to the Grove",
-    //     paragraph:
-    //       "Silent Walk + Social Gathering",
-    //     image: "/images/blog/blog0006/blog6.webp",
-    //     alt: "Beautiful Woodland Path",
-    //     width: 500,
-    //     height: 375,
-    //     tags: ["EVENT"],
-    //     publishDate: "October 3, 2024",
-    //     blogURL: "/blog/posts/blogPage0006"
-    // },
+    {
+        id: 7,
+        title: "Not a Done Deal!",
+        paragraph:
+          "We don’t want a housing development beside and cutting into the Grove.",
+        image: "/images/blog/blog0007/blog7.png",
+        alt: "People walking a woodland path",
+        width: 500,
+        height: 270,
+        tags: ["UPDATE"],
+        publishDate: "October 17, 2024",
+        blogURL: "/blog/posts/post-2024-10-17-00"
+    },
+    {
+        id: 6,
+        title: "Giving Thanks to the Grove",
+        paragraph:
+          "Silent Walk + Social Gathering",
+        image: "/images/blog/blog0006/blog6.webp",
+        alt: "Beautiful Woodland Path",
+        width: 500,
+        height: 375,
+        tags: ["EVENT"],
+        publishDate: "October 3, 2024",
+        blogURL: "/blog/posts/ost-2024-10-03-00"
+    },
   
-    // {
-    //     id: 5,
-    //     title: "Expert Opinion on Proposed Development",
-    //     paragraph:
-    //       "Alberto Suárez-Esteban, Ph.D in Biology with a concentration in Plant Ecology and Conservation, M.Sc in Biodiversity, B.Sc.in Biology, speaks at the STGA Launch event on Saturday, September 7, 2024",
-    //     image: "/images/blog/blog0005/blog5.jpg",
-    //     alt: "Human and dog walk along beautiful wooded trail with signage, Hemlock Loop",
-    //     width: 552,
-    //     height: 296,
-    //     tags: ["Expert Opinion"],
-    //     publishDate: "September 20, 2024",
-    //     blogURL: "/blog/posts/blogPage0005"
-    // },
+    {
+        id: 5,
+        title: "Expert Opinion on Proposed Development",
+        paragraph:
+          "Alberto Suárez-Esteban, Ph.D in Biology with a concentration in Plant Ecology and Conservation, M.Sc in Biodiversity, B.Sc.in Biology, speaks at the STGA Launch event on Saturday, September 7, 2024",
+        image: "/images/blog/blog0005/blog5.jpg",
+        alt: "Human and dog walk along beautiful wooded trail with signage, Hemlock Loop",
+        width: 552,
+        height: 296,
+        tags: ["Expert Opinion"],
+        publishDate: "September 20, 2024",
+        blogURL: "/blog/posts/post-2024-09-20-00"
+    },
    
     {
         id: 4,
@@ -226,7 +226,7 @@ export const blogData = [
         height: 500,
         tags: ["Important News"],
         publishDate: "September 18, 2024",
-        blogURL: "/blog/posts/blogPage0004"
+        blogURL: "/blog/posts/post-2024-09-18-00"
     },
     {
         id: 3,
@@ -252,7 +252,7 @@ export const blogData = [
         height: 375,
         tags: ["Update"],
         publishDate: "September 11, 2024",
-        blogURL: "/blog/posts/blogPage0002"
+        blogURL: "/blog/posts/post-2024-09-11-00"
     },
     {
         id: 1,
