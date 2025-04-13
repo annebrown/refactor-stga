435f680cbef41c587331c66f14d29116e71ce0fc
Anne Brown <anne@browndomain.com>
2025-04-13 08:04:36 -0400

    Donations Images Getting Corrupted

---
+++ app/pages/blog/posts/post-2025-01-18-00.vue
@@ -0,0 +1,101 @@
+<!--------@pages/blog/posts/post-2025-12-18-00.vue----------------------------->
+<template><div>
+
+<img
+    src="/images/blog/blog0013/blog13.png"
+    alt="Greetings from Gillies Grove, 10,000 years in the making"
+    width="500"
+    height="386"
+        class="w-full mx-auto mb-10 px-10"
+/>
+
+<p  class="pb-1"><strong>When:</strong> NOW</p>
+
+<p  class="mb-0 pb-0">
+    <strong>Pickup Locations:</strong>
+</p>
+
+<ul  class="ml-2 mb-5">
+    <li><a href="http://cafetilda.ca/"  class="text-green-600 hover:text-primary">Cafe Tilda</a>, 158 John St. N.</li>
+    <li><a href="https://www.instagram.com/johnies.vintage/"  class="text-green-600 hover:text-primary">Johnie&apos;s Vintage</a>, 54 Madawaska St.</li>
+    <li><a href="http://www.mallardsmilkbar.com/"  class="text-green-600 hover:text-primary">Mallard&apos;s Milk Bar</a>, 55 John St. N.</li>
+    <li><a href="http://www.ottawavalleycoffee.ca/"  class="text-green-600 hover:text-primary">Ottawa Valley Coffee</a>, 32 Elgin St. W.</li>
+    <li>Sweet & Sassy Cafe and Bistro, 114 John St. N.</li>
+    <li><a href="https://www.whitepinebooks.ca/"  class="text-green-600 hover:text-primary">White Pine Books</a>, 152 John St. N.</li>
+    <li><a href="https://ovap.ca/"  class="text-green-600 hover:text-primary">Ottawa Valley Air Paddle</a>, 67 Madawaska St. N.</li>
+    <li>(more to come)</li>
+</ul>
+
+<p  class="mb-8 text-justify">
+    Let our town know how you feel about the Grove and surrounding lands. While the old trees may still be standing, the crucial ecotone and supporting habitats that surround it are slated for development, ultimately putting the endangered old-growth woodland at risk. 
+</p>
+
+<p  class="mb-4 font-bold">
+Did you know?
+</p>
+<p  class="mb-0 text-blue-800 text-justify">
+    “Ninety-eight percent of North America&apos;s old-growth forests have been eradicated.”
+</p>
+<p  class="mb-8 text-sm">
+    - from Ontario&apos;s Old-Growth Forests by <em>Michael Henry</em> and <em>Peter Quinby</em>
+</p>
+
+<p  class="mb-8 text-justify">
+    Both the Provincial Policy Statement and the Town of Arnprior Official Plan emphasize the need to protect significant woodlands AND their adjacent lands. Why then is development being considered for this environmentally sensitive area? 
+    Let our elected officials know how you feel about this.
+</p>
+<p  class="mb-8 text-green-700 text-justify">
+    ***Request that members of council initiate a thorough ecological assessment of Gillies Grove Nature Reserve and the adjacent lands.*** 
+</p>
+
+<p  class="mb-8 text-justify">
+    Pick up a postcard from one of the above listed local businesses. We have three design options to choose from:
+</p>
+
+<ul  class="mb-8">
+    <li>🍄 Grab a blank one and write your own message</li>
+    <li>🍄 Take one with a pre-written message and add your name and postal code</li>
+    <li>🍄 Kids can draw themselves in the Grove in our special child friendly version</li>
+</ul>
+
+
+    <img
+        src="/images/blog/blog0013/01.webp"
+        alt="Greetings from Gillies Grove, 10,000 years in the making"
+        width="386"
+        height="500"
+         class="inline-block self-center w-full mb-0 pb-8 !mx-auto"
+    />
+
+
+<div  class="w-full mt-0 mb-8 pt-0 mx-auto">
+    <img
+        src="/images/blog/blog0013/03.webp"
+        alt="Kids Page"
+        width="500"
+        height="386"
+         class="inline-block self-center w-full pb-8 mx-auto"
+    />
+</div>
+
+<p  class="mb-8 text-justify">
+    Then either drop it in the mailbox (you&apos;ll need postage), drop it in the town&apos;s mail slot (105 Elgin St. W.), or perhaps request a meeting with one of our councillors and deliver it in person. Hey, snap a selfie of you posting your card in the mail slot and share it with our socials - let&apos;s get the word out…
+</p>
+
+<p  class="mb-8 text-justify">
+    In addition to joining our postcard campaign, please consider supporting this cause in the following ways:
+</p>
+
+<ul  class="mb-8">
+    <li>💚 Invite family and friends</li>
+    <li>💚 Wear a button - Get yours at Green Bean Natural Foods (117 John St. N.) or Sweet and Sassy (114 John St. N.)</li>
+    <li>💚 Talk to your friends and family about the importance of old-growth forests </li>
+    <li>💚 Subscribe to our <a  class="text-green-600" href="https://docs.google.com/forms/d/e/1FAIpQLSfBFJHDmUnmpGv1XaSHGaSSPmGizbxrQ-KgeqRRZqdVNZeuHg/viewform">mailing list</a>.</li>
+    <li>💚 Fill out our <a   class="text-green-600" href="https://docs.google.com/forms/d/e/1FAIpQLSc611weSVb3qvnl6KJd6AZ_lmRn0yStEYDpxIkE9bVJhFTeUQ/viewform?usp=sf_link"> volunteer form</a></li>
+    <li>💚 Like and follow our <a  class="text-green-600" href="https://www.facebook.com/people/Save-the-Grove-Again/61565284064994/">Facebook page</a></li>
+    <li>💚 Subscribe to our <a  class="text-green-600" href="https://www.youtube.com/@SaveTheGroveAgain">YouTube channel</a></li>
+    <li>💚 Make a donation via e-transfer to  <a href="mailto:savethegroveagain@gmail.com"  class="text-green-600 hover:text-primary">savethegroveagain@gmail.com</a></li>
+</ul>
+
+ </div></template>
+ <!--------@pages/blog/posts/post-2025-12-18-00.vue----------------------------->
