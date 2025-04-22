e63074ea5adcf7c1540e5f5be27d335eaf6efc1e
Anne Brown <anne@browndomain.com>
2025-04-22 08:36:05 -0400

    Color

--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -5,7 +5,7 @@
 <!-- All Blogs -->
    <!-- All Posts -->
    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-8 p-4 rounded-4xl
-        w-full bg-(--ui-bg) dark:bg-(--ui-primary)
+        w-full bg-(--ui-bg) dark:bg-(--ui-primary-2)
         place-content-stretch">
 
   <!-- Rounded bottom with thicker primary-colored top border -->
