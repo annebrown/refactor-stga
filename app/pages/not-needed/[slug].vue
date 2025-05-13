549bedee59323cd253c43c79acc65b913651fa27
Anne Brown <anne@browndomain.com>
2025-05-13 11:47:28 -0400

    No prose

--- app/pages/not-needed/[slug].vue
+++ app/pages/not-needed/[slug].vue
@@ -3,7 +3,7 @@
     <h1>{{ post.title }}</h1>
     <p class="text-sm text-gray-500">{{ new Date(post.date).toLocaleDateString() }}</p>
     <img v-if="post.featured_image" :src="post.featured_image" alt="Featured Image" class="my-4 rounded-md">
-    <div v-html="post.content" class="prose"></div>
+    <div v-html="post.content" class=""></div>
   </div>
   <div v-else>
     Loading post...
